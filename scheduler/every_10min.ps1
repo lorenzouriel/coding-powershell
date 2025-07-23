4e160441f7dbcd5510787b47b8f70affa21236d7
Lorenzo Uriel <lorenzo.uriel@geosiga.com.br>
2025-07-23 11:44:48 -0300

    feat: updated argument scheduler every 05 min

--- scheduler/every_10min.ps1
+++ scheduler/every_10min.ps1
@@ -1,15 +1,16 @@
-﻿$Action = New-ScheduledTaskAction -Execute 'Example';
+﻿$Action = New-ScheduledTaskAction -Execute 'C:\Program Files\Microsoft SQL Server\150\DTS\Binn\DTExec.exe' `
+  -Argument '/F "C:\SSIS\job.dtsx" /Decrypt "Password"';
 
 $startTime = [datetime]::Today
 
 $Trigger = New-ScheduledTaskTrigger -Once -At $startTime `
-  -RepetitionInterval (New-TimeSpan -Minutes 5) `
+  -RepetitionInterval (New-TimeSpan -Minutes 10) `
   -RepetitionDuration (New-TimeSpan -Days 360)
 
 $Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest;
 
 Register-ScheduledTask `
-  -TaskName "Every05Min" `
+  -TaskName "Every10Min" `
   -Action $Action `
   -Trigger $Trigger `
   -Principal $Principal `
