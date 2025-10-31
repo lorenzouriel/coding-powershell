49f5ffb1b8825d6386d8af95be97725b6dad1d58
Lorenzo Uriel <lorenzo.uriel@geosiga.com.br>
2025-10-30 21:53:22 -0300

    feat: add to gac option

---
+++ helps/register_gac_wout_sdk.ps1
@@ -0,0 +1,12 @@
+﻿# Load the EnterpriseServices assembly manually
+Add-Type -AssemblyName "System.EnterpriseServices"
+
+# Now create the Publish object
+$publish = New-Object System.EnterpriseServices.Internal.Publish
+
+# Install your assembly into the GAC
+$assemblyPath = "C:\repos\integration-enel\ETL\assemblies\Newtonsoft.Json.dll"
+$publish.GacInstall($assemblyPath)
+
+# (Optional) Verify
+$publish.GacList("Newtonsoft.Json")
